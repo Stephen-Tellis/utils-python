e8bee236c2d1240b595d2793a3d86780ec89b727
Stephen-Tellis <59031130+Stephen-Tellis@users.noreply.github.com>
2021-12-23 06:03:29 +0100

    Create logger_config.py

---
+++ logger_config.py
@@ -0,0 +1,34 @@
+"""! @brief A utility that sets up logging for us."""
+##
+# @section description_logger_config Description
+# This file sets up logger formats, level etc
+# Change level/format/add logfile here to reflect accross all classes in your package
+#
+##
+# @file logger_config.py
+#
+# @section libraries_logger_config Libraries/Modules
+# - logging 
+#
+# @section author_logger_config Author(s)
+# - Created by Stephen Tellis.
+#
+
+import logging
+
+# Setup Logging
+def setup_logger(name):
+    """!
+    Returns a logger object for the file.
+
+    @param name Pass the __name__ of the file you desire to setup logging on
+
+    @return logger object
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("""[%(levelname)s] [%(asctime)s] [%(filename)s]: %(message)s""")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
