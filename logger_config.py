87f553a6107e6b985fc6c12b3da4ba0b8bff7c0c
Stephen Tellis <59031130+Stephen-Tellis@users.noreply.github.com>
2022-05-01 11:16:25 +0200

    Included file handler within logger

--- logger_config.py
+++ logger_config.py
@@ -17,12 +17,12 @@
 import logging
 
 # Setup Logging
-def setup_logger(name):
+def setup_logger(name, filename=None):
     """!
     Returns a logger object for the file.
-
     @param name Pass the __name__ of the file you desire to setup logging on
-
+    @param filename Full filepath to the log file
+    
     @return logger object
     """
     logger = logging.getLogger(name)
@@ -30,5 +30,9 @@ def setup_logger(name):
     formatter = logging.Formatter("""[%(levelname)s] [%(asctime)s] [%(filename)s]: %(message)s""")
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
+    if isinstance(filename, str):
+        file_handler = logging.FileHandler(filename)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
     return logger
